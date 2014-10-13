ca5ea3990c2597ce2d17b432f8f1a102a82b2bf0
Kingroka <hunterten1@gmail.com>
2014-10-12 20:44:33 -0400

    Added support for variables in conditional statements

--- RML/src/com/gh/cmds/If.java
+++ RML/src/com/gh/cmds/If.java
@@ -9,30 +9,43 @@ import javax.script.ScriptException;
 import com.gh.main.Command;
 import com.gh.main.Script;
 
-public class If extends Command{
+public class If extends Command {
 	String condition;
 	public ArrayList<Command> cmds = new ArrayList<Command>();
 	ScriptEngineManager factory = new ScriptEngineManager();
-    ScriptEngine engine = factory.getEngineByName("JavaScript");
+	ScriptEngine engine = factory.getEngineByName("JavaScript");
+	Script script;
 
 	public If(Script script, String cond) {
 		condition = cond;
+		this.script = script;
 	}
-	public void execute(){
-		 try {
-				Boolean bool = (Boolean) engine.eval(condition);
-				if(bool){
-					for (int i = 0; i < cmds.size(); i++) {
-						cmds.get(i).execute();
-					}
+
+	public void execute() {
+		convertToJavaScript();
+		try {
+			Boolean bool = (Boolean) engine.eval(condition);
+			if (bool) {
+				for (int i = 0; i < cmds.size(); i++) {
+					cmds.get(i).execute();
 				}
-			} catch (ScriptException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	public void convertToJavaScript(){
-		
+
+	public void convertToJavaScript() {
+		for (int i = 0; i < script.variables.size(); i++) {
+			if (condition.contains(script.variables.get(i).getName())) {
+				String varName = script.variables.get(i).getName();
+
+				condition = condition.replaceAll(varName,
+						script.variables.get(i).getValue().toString());
+
+			}
+		}
 	}
 
 }
